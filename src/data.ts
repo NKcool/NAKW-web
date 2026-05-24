411355db21569a436fbbaafb74fb470215b4e4c9
Naveen <nkcool3956@gmail.com>
2026-05-24 13:16:46 +0530

    final cahnges

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,7 @@ export const PROJECTS = [
     id: 'bindu-ai',
     title: 'Bindu AI - Personal Voice Assistant',
     description: 'Built Bindu AI, a personal AI assistant, using Python scripting and integration with the OpenAI API that uses 175B parameters. Bindu AI operates with voice commands and performs tasks according to user instructions including 15+ custom commands. It offers a range of functionalities, including answering questions, providing weather updates, and is available 24/7 for your service.',
-    image: '/bindu-ai.png',
+    image: 'bindu-ai.png',
     link: '#',
     tags: ['Python', 'OpenAI API', 'Voice Automation']
   },
@@ -11,7 +11,7 @@ export const PROJECTS = [
     id: 'insta-reel-updater',
     title: 'Insta Reel Updater',
     description: 'An AI-powered automation tool designed to efficiently update and manage Instagram Reels programmatically. Streams workflows for content creators and marketers saving countless hours of manual work.',
-    image: '/insta-reel.png',
+    image: 'insta-reel.png',
     link: 'https://insta-reel-updater.onrender.com/',
     tags: ['Python', 'Automation', 'Social Media API']
   },
@@ -19,7 +19,7 @@ export const PROJECTS = [
     id: 'lang-learning',
     title: 'Language Learning Website',
     description: 'Created a feature-rich language learning platform, including user-friendly quizzes, secure account authentication, and an engaging leaderboard to foster healthy competition. Resulted in a 20% increase in retention and a 25% boost in mobile engagement.',
-    image: '/lang-learning.png',
+    image: 'lang-learning.png',
     link: 'https://languagelearning.onrender.com/',
     tags: ['Web Development', 'Auth', 'Gamification']
   },
@@ -27,7 +27,7 @@ export const PROJECTS = [
     id: 'image-gallery',
     title: 'Image Gallery',
     description: 'Explore a world of stunning visuals with our Image Gallery app, using the Unsplash API. Discover high-quality, royalty-free images effortlessly. Your gateway to endless inspiration and beautiful wallpapers.',
-    image: '/image-gallery.png',
+    image: 'image-gallery.png',
     link: 'https://nkcool.github.io/ImageGallery/',
     tags: ['API Integration', 'UI/UX']
   },
@@ -35,7 +35,7 @@ export const PROJECTS = [
     id: 'farm-rent',
     title: 'Farm Rent (Group Project)',
     description: 'Access essential farm equipment effortlessly with our app. Browse, compare, and rent tractors and more, fostering efficiency in agriculture. Say goodbye to ownership hassles and embrace a flexible, cost-effective approach to farming.',
-    image: '/farm-rent.png',
+    image: 'farm-rent.png',
     link: 'https://devendradhote.github.io/Hackathon_codebite/',
     tags: ['Web App', 'Team Project', 'Marketplace']
   }
